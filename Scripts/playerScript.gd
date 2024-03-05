153868404e60df4fbafabb2403123a33c5fd35b8
BBroOW <brage_hagelund@outlook.com>
2024-03-05 21:20:35 +0100

    Remove Level1 scene, update green material, and modify playerScript

--- Scripts/playerScript.gd
+++ Scripts/playerScript.gd
@@ -10,11 +10,13 @@ var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var camera := $Neck/Camera3D
 
 @onready var floorMesh := $".."/Bakke
+@onready var deathZone := $".."/DeathZone
 
 var matGreen = preload("res://Matirials/green.tres")
 var matRed = preload("res://Matirials/red.tres")
 
 
+#Movement
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -32,9 +34,7 @@ func _physics_process(delta: float) -> void:
 
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		floorMesh.set_surface_override_material(0,matRed)
-	else:
-		floorMesh.set_surface_override_material(0,matGreen)
+
 		
 	
 	if Input.is_action_pressed("ui_accept") and is_on_floor():
@@ -58,3 +58,15 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+#Death
+
+
+	
+
+
+	
+	
+
+
+func _on_death_zone_body_shape_entered(body_rid:RID, body:Node3D, body_shape_index:int, local_shape_index:int):
+	transform.origin = Vector3(0, 1, 0)
